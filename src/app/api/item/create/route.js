b17b91ff395a6f86d5c41acc97e2e98250001fb1
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-10-08 18:33:06 +0900

    feat: アイテムの作成

---
+++ src/app/api/item/create/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import supabase from "@/utils/database"
+
+export async function POST(request){
+    const reqBody = await request.json()
+    console.log(reqBody)
+    try {
+        const { error } = await supabase.from("items").insert(reqBody)
+        console.log(error)
+        if(error) throw new Error()
+        return NextResponse.json({message: "アイテム作成成功"})
+    } catch {
+        return NextResponse.json({message: "アイテム作成失敗"})
+    }
+};
