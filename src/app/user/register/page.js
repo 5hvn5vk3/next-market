f960b869ca3dea24325e40b308299fce3cbbc3ea
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-10-13 19:05:21 +0900

    refactor: ユーザー登録ページでデータを1つのstateにまとめた

--- src/app/user/register/page.js
+++ src/app/user/register/page.js
@@ -1,9 +1,17 @@
 "use client";
 import { useState } from "react";
 const Register = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [newUser, setNewUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+    const handleChange = (e) => {
+        setNewUser({
+            ...newUser,
+            [e.target.name]: e.target.value,
+        });
+    };
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -15,11 +23,7 @@ const Register = () => {
                         Accept: "application/json",
                         "Content-Type": "application/json",
                     },
-                    body: JSON.stringify({
-                        name: name,
-                        email: email,
-                        password: password,
-                    }),
+                    body: JSON.stringify(newUser),
                 }
             );
             const jsonData = await response.json();
@@ -33,23 +37,26 @@ const Register = () => {
             <h1>ユーザー登録</h1>
             <form onSubmit={handleSubmit}>
                 <input
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
+                    value={newUser.name}
+                    onChange={handleChange}
                     type="text"
+                    name="name"
                     placeholder="名前"
                     required
                 />
                 <input
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    value={newUser.email}
+                    onChange={handleChange}
                     type="text"
+                    name="email"
                     placeholder="メールアドレス"
                     required
                 />
                 <input
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    value={newUser.password}
+                    onChange={handleChange}
                     type="text"
+                    name="password"
                     placeholder="パスワード"
                     required
                 />
