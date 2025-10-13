ddbd131fc907b96a5db5f9828839227b2293ccef
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-10-13 18:35:06 +0900

    remove: バックエンド側のトークンのコンソール出力の削除

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -1,34 +1,43 @@
-import { NextResponse } from "next/server"
-import { SignJWT } from "jose"
-import supabase from "../../../utils/database"
-export async function POST(request){
-    const reqBody = await request.json()
-    try{
-        const { data, error } = await supabase.from("users").select().eq("email", reqBody.email).single()
-        console.log(data)
-        if(!error){
+import { NextResponse } from "next/server";
+import { SignJWT } from "jose";
+import supabase from "../../../utils/database";
+export async function POST(request) {
+    const reqBody = await request.json();
+    try {
+        const { data, error } = await supabase
+            .from("users")
+            .select()
+            .eq("email", reqBody.email)
+            .single();
+        console.log(data);
+        if (!error) {
             // ユーザーデータが存在する場合の処理
-            if(data.password === reqBody.password){
+            if (data.password === reqBody.password) {
                 // パスワードが正しい場合の処理
-                const secretKey = new TextEncoder().encode("next-market-route-handlers")
+                const secretKey = new TextEncoder().encode(
+                    "next-market-route-handlers"
+                );
                 const payload = {
-                    email: reqBody.email
-                }
+                    email: reqBody.email,
+                };
                 const token = await new SignJWT(payload)
                     .setProtectedHeader({ alg: "HS256" })
                     .setExpirationTime("1d")
-                    .sign(secretKey)
-                console.log(token) // 本当にトークンが発行されるのかの確認
-                return NextResponse.json({message: "ログイン成功", token})
-            }else{
+                    .sign(secretKey);
+                return NextResponse.json({ message: "ログイン成功", token });
+            } else {
                 // パスワードが間違っている場合の処理
-                return NextResponse.json({message: "ログイン失敗：パスワードが間違っています"})
+                return NextResponse.json({
+                    message: "ログイン失敗：パスワードが間違っています",
+                });
             }
-        }else{
+        } else {
             // ユーザーデータが存在しない場合の処理
-            return NextResponse.json({message: "ログイン失敗：ユーザー登録をしてください"})
+            return NextResponse.json({
+                message: "ログイン失敗：ユーザー登録をしてください",
+            });
         }
-    }catch{
-        return NextResponse.json({message: "ログイン失敗"}) 
+    } catch {
+        return NextResponse.json({ message: "ログイン失敗" });
     }
 }
