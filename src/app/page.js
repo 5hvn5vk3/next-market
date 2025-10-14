297ae217bf783f84ccddf6da11acb4345eed1503
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-10-14 16:18:16 +0900

    feat: すべてのアイテムを読み取るページ

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
-"use client";
-import * as highlightjs from "highlight.js";
-const ReadAllItems = () => {
+export const dynamic = "force-dynamic";
+const getAllItems = async () => {
+    const response = await fetch("http://localhost:3000/api/item/readall");
+    const jsonData = await response.json();
+    const allItems = jsonData.allItems;
+    return allItems;
+};
+const ReadAllItems = async () => {
+    const allItems = await getAllItems();
     return (
         <div>
-            <h1 className="h1-style">こんにちは</h1>
-            <h3>さようなら</h3>
+            {allItems.map((item) => (
+                <div key={item.id}>
+                    <img src={item.image} />
+                    <p>{item.price}</p>
+                    <p>{item.title}</p>
+                    <p>{item.description}</p>
+                </div>
+            ))}
         </div>
     );
 };
