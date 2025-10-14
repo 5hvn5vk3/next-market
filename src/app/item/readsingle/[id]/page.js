6fb2c1c84f802d7f073e0916324079ef74be8ca2
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-10-14 17:25:39 +0900

    feat: すべてのアイテムを読み取るページ

---
+++ src/app/item/readsingle/[id]/page.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+const getSingleItem = async (id) => {
+    const response = await fetch(
+        `http://localhost:3000/api/item/readsingle/${id}`
+    );
+    const jsonData = await response.json();
+    const singleItem = jsonData.singleItem;
+    return singleItem;
+};
+const ReadSingleItem = async (context) => {
+    const params = await context.params;
+    const singleItem = await getSingleItem(params.id);
+    return (
+        <div>
+            <Image
+                src={singleItem.image}
+                width={750}
+                height={500}
+                alt="item-image"
+                priority
+            />
+            <p>{singleItem.price}</p>
+            <p>{singleItem.title}</p>
+            <p>{singleItem.description}</p>
+        </div>
+    );
+};
+
+export default ReadSingleItem;
