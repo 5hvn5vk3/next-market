7c3bfa2813507896f93a69717d4e9cb0a782a0e7
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-10-14 17:23:52 +0900

    update: 画像表示のタグを変更

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export const dynamic = "force-dynamic";
 const getAllItems = async () => {
     const response = await fetch("http://localhost:3000/api/item/readall");
@@ -11,7 +12,12 @@ const ReadAllItems = async () => {
         <div>
             {allItems.map((item) => (
                 <div key={item.id}>
-                    <img src={item.image} />
+                    <Image
+                        src={item.image}
+                        width={750}
+                        height={500}
+                        alt="item-image"
+                    />
                     <p>{item.price}</p>
                     <p>{item.title}</p>
                     <p>{item.description}</p>
