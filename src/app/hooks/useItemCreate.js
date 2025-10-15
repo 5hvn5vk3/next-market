762eb935062432b5935f1ee44d3865b838dcc9a5
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-10-15 15:27:08 +0900

    refactor: アイテム追加ページでカスタムフックを使用

---
+++ src/app/hooks/useItemCreate.js
@@ -0,0 +1,55 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export const useItemCreate = () => {
+    const [formData, setFormData] = useState({
+        title: "",
+        price: "",
+        image: "",
+        description: "",
+    });
+
+    const router = useRouter();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(
+                "http://localhost:3000/api/item/create",
+                {
+                    method: "POST",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${localStorage.getItem(
+                            "token"
+                        )}`,
+                    },
+                    body: JSON.stringify({
+                        title: formData.title,
+                        price: formData.price,
+                        image: formData.image,
+                        description: formData.description,
+                        email: "ダミーデータ",
+                    }),
+                }
+            );
+            const jsonData = await response.json();
+            alert(jsonData.message);
+            router.push("/");
+        } catch (err) {
+            alert("アイテム作成失敗");
+        }
+    };
+    return {
+        formData,
+        handleChange,
+        handleSubmit,
+    };
+};
--- src/app/item/create/page.js
+++ src/app/item/create/page.js
@@ -1,53 +1,8 @@
 "use client";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useItemCreate } from "@/hooks/useItemCreate";
 
 const CreateItem = () => {
-    const [formData, setFormData] = useState({
-        title: "",
-        price: "",
-        image: "",
-        description: "",
-    });
-
-    const router = useRouter();
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch(
-                "http://localhost:3000/api/item/create",
-                {
-                    method: "POST",
-                    headers: {
-                        Accept: "application/json",
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${localStorage.getItem(
-                            "token"
-                        )}`,
-                    },
-                    body: JSON.stringify({
-                        title: formData.title,
-                        price: formData.price,
-                        image: formData.image,
-                        description: formData.description,
-                        email: "ダミーデータ",
-                    }),
-                }
-            );
-            const jsonData = await response.json();
-            alert(jsonData.message);
-            router.push("/");
-        } catch (err) {
-            alert("アイテム作成失敗");
-        }
-    };
-
+    const { formData, handleChange, handleSubmit } = useItemCreate();
     return (
         <div>
             <h1 className="page-title">アイテム作成</h1>
