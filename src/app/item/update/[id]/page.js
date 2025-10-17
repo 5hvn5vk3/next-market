eb4a36e186fe4fadd0c6a64be2f9504ed9c21ca7
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-10-17 15:28:37 +0900

    update: 編集ページのhooksを削除し、ReactサーバーコンポーネントとReactバージョン19の機能を導入

--- src/app/item/update/[id]/page.js
+++ src/app/item/update/[id]/page.js
@@ -1,56 +1,15 @@
-"use client";
-import useAuth from "@/utils/useAuth";
-import useItemUpdate from "@/hooks/useItemUpdate";
+import Form from "./form";
+import { getSingleItem } from "../../readsingle/[id]/page";
 
-const UpdateItem = (context) => {
-    const loginUserEmail = useAuth();
-    const { item, handleChange, handleSubmit } = useItemUpdate(
-        context.params.id,
-        loginUserEmail
+const UpdateItem = async (context) => {
+    const params = await context.params;
+    const singleItem = await getSingleItem(params.id);
+    return (
+        <div>
+            <h1 className="page-title">アイテム編集</h1>
+            <Form params={params} singleItem={singleItem} />
+        </div>
     );
-
-    if (loginUserEmail === item.email) {
-        return (
-            <div>
-                <h1 className="page-title">アイテム編集</h1>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        value={item.title}
-                        onChange={handleChange}
-                        type="text"
-                        name="title"
-                        placeholder="アイテム名"
-                        required
-                    />
-                    <input
-                        value={item.price}
-                        onChange={handleChange}
-                        type="text"
-                        name="price"
-                        placeholder="価格"
-                        required
-                    />
-                    <input
-                        value={item.image}
-                        onChange={handleChange}
-                        type="text"
-                        name="image"
-                        placeholder="画像"
-                        required
-                    />
-                    <textarea
-                        value={item.description}
-                        onChange={handleChange}
-                        name="description"
-                        rows={15}
-                        placeholder="商品説明"
-                        required
-                    ></textarea>
-                    <button>編集</button>
-                </form>
-            </div>
-        );
-    }
 };
 
 export default UpdateItem;
