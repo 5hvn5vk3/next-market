861d6ef0c954950704a5e759f9eba1f2d47f18a5
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-10-17 16:34:34 +0900

    修正し忘れていたURLの修正

--- src/app/hooks/useItemCreate.js
+++ src/app/hooks/useItemCreate.js
@@ -23,7 +23,7 @@ export const useItemCreate = () => {
         e.preventDefault();
         try {
             const response = await fetch(
-                "http://localhost:3000/api/item/create",
+                `${process.env.NEXT_PUBLIC_URL}/api/item/create`,
                 {
                     method: "POST",
                     headers: {
