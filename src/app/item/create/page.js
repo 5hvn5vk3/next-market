0c960e8096b449b8fdff55dcbc40ebcca4acf212
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-10-18 11:11:46 +0900

    update: お手本との差分を見るためにカスタムフックを削除

--- src/app/item/create/page.js
+++ src/app/item/create/page.js
@@ -1,9 +1,49 @@
 "use client";
-import { useItemCreate } from "@/hooks/useItemCreate";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import useAuth from "@/utils/useAuth";
 
 const CreateItem = () => {
-    const { formData, loginUserEmail, handleChange, handleSubmit } =
-        useItemCreate();
+    const [formData, setFormData] = useState({
+        title: "",
+        price: "",
+        image: "",
+        description: "",
+    });
+
+    const router = useRouter();
+    const loginUserEmail = useAuth();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("/api/item/create", {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+                body: JSON.stringify({
+                    title: formData.title,
+                    price: formData.price,
+                    image: formData.image,
+                    description: formData.description,
+                    email: loginUserEmail,
+                }),
+            });
+            const jsonData = await response.json();
+            alert(jsonData.message);
+            router.push("/");
+        } catch (err) {
+            alert("アイテム作成失敗");
+        }
+    };
     if (loginUserEmail) {
         return (
             <div>
